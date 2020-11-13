ef307ecada334726b313edfaa612e8d0d87efb12
Armanchik74 <72072914+Armanchik74@users.noreply.github.com>
2020-11-13 09:40:36 +0500

    Add files via upload

---
+++ select.py
@@ -0,0 +1,30 @@
+import random
+
+dim = 20
+arr = [random.randint(0, 100) for i in range(dim)]
+
+print("Исходный массив")
+print(arr)
+ 
+k = 0
+mcount = 0
+ccount = 0
+
+for k in range(0, dim - 1):  
+        m = k  
+        i = k + 1 
+        for i in range(i, dim):
+            ccount += 1
+            if arr[i] < arr[m]:
+                m = i
+        if k != m:
+            t = arr[k]
+            arr[k] = arr[m]
+            arr[m] = t
+            mcount += 1
+          
+print("\nУпорядоченный массив: метод простого выбора")
+print(arr)
+print("\nЭлементов в массиве: ", dim)
+print("Сравнений: ", ccount)
+print("Перестановок: ", mcount)
---
+++ Метод Пузырька.py
@@ -0,0 +1,24 @@
+import random
+
+dim = 20
+arr = [random.randint(0, 100) for i in range(dim)]
+
+print("Исходный массив")
+print(arr)
+
+n = 1
+alg_count = [0, 0]
+while n < dim:
+        for i in range(dim - n):
+            alg_count[0] += 1
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                alg_count[1] += 1
+        n += 1
+
+ 
+print("\nУпорядоченный массив: метод простого выбора")
+print(arr)
+print("\nЭлементов в массиве: ", dim)
+print("Сравнений: ", alg_count[1])
+print("Перестановок: ", alg_count[0])
